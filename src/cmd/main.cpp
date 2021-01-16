c81a7932dc170641bf8a1e89e00a2a2f3d46c26e
Ivan Avramov <iavramov@futurewei.com>
2021-01-16 12:48:45 -0800

    building

--- src/cmd/main.cpp
+++ src/cmd/main.cpp
@@ -1,22 +1,42 @@
 #include <string>
 #include <pthread.h>
 
-#include "logging/log.h"
+#include <logging/log.h>
+#include <logging/json.h>
+
 k2::Logger logger("LMAIN");
 
-// MAC
-// no-log: 2ns/call
-// log with disabled level: 3.35ns/call
-// log "" to cout: 1321ns/call
-// log "" to cout no flush: 838ns/call
-// log "" to clog: 6338ns/call
-// log "" to cerr: 8101ns/call
-// log fmt "" to cout w/ flush: 817ns/call
-// log fmt "" to cout w/o flush: 325ns/call
-// log fmt "int - int str" to cout w/o flush: 368ns/call
-// log fmt "int - int str" to cout w   flush: 849ns/call
-// log stream "int - int str" to cout w   flush: 1795/call
-// log stream "int - int str" to cout w/o flush: 1228/call
+struct Data {
+    std::string str1;
+    std::string str2;
+    uint64_t int1;
+    uint64_t int2;
+    inline friend std::ostream& operator<<(std::ostream& os, const Data& d) {
+        return os << "{\"str1\": " << d.str1 << ", \"str2\": " << d.str2 << ", \"int1\": " << d.int1 << ", \"int2\": " << d.int2 << "}";
+    }
+    inline friend void to_json(nlohmann::json& j, const Data& d) {
+        j = nlohmann::json{
+            {"str1", d.str1},
+            {"str2", d.str2},
+            {"int1", d.int1},
+            {"int2", d.int2}
+        };
+    }
+};
+
+template <>
+struct fmt::formatter<Data> {
+    template <typename ParseContext>
+    constexpr auto parse(ParseContext& ctx) {
+        return ctx.begin();
+    }
+
+    template <typename FormatContext>
+    auto format(Data const& d, FormatContext& ctx) {
+        return fmt::format_to(ctx.out(), FMT_COMPILE("{{\"str1\":{}, \"str2\":{}, \"int1\":{}, \"int2\":{}}}"),
+            d.str1, d.str2, d.int1, d.int2);
+    }
+};
 
 inline void pin(int cpu) {
     cpu_set_t cpuset;
@@ -27,20 +47,21 @@ inline void pin(int cpu) {
     }
 }
 
-uint64_t dowork(uint64_t a, uint64_t i) {
-    //LOG_DEBUG("got" << i << " -- " << a);
-    //LSDEBUG(logger, a << " - " << i << " ");
-    LFDEBUG(logger, "{} - {} {}", a, i, "");
-    LFDEBUG(logger, "no");
-    //LLOG_DEBUG("got" << i << a);
-    //logger.D([&](auto& os) { os << "got:" << i << "--" << a; });
-    //LDEBUG(logger, "got: {} -- {}", i, a);
+uint64_t dowork(uint64_t a, uint64_t i, Data& d) {
+    LSDEBUG(logger, "obj=" << d);
+    //LFDEBUG(logger, "obj={}", d);
     return (a + i) / a;
 }
-uint64_t bench(uint64_t a) {
+
+uint64_t bench(uint64_t a, std::string str) {
     uint64_t result = a;
+    Data d;
+    d.str1 = str + "ab";
+    d.str2 = str + "cd";
+    d.int1 = 18446744073709551615ull - a;
+    d.int2 = 18446744073709551615ull - a + 1;
     for (uint64_t i = 0; i < a; ++i) {
-        result += dowork(a, i);
+        result += dowork(a, i, d);
     }
     return result;
 }
@@ -50,13 +71,10 @@ int main(int, char** argv) {
     auto c = std::stoull(argv[1]);
     k2::LOG_LEVEL = (k2::LogLevel)std::stoi(argv[2]);
     LSINFO(logger, "main: " << c);
-    LSDEBUG(logger, "deb");
-    LSERROR(logger, "err");
     auto st = k2::Clock::now();
-    auto result = bench(c);
+    auto result = bench(c, std::string(30, argv[1][0]));
     auto end = k2::Clock::now();
     auto el = end - st;
-    LSINFO(logger, "res= " << result << ", elapsed=" << el << ", percall=" << ((double)k2::nsec(el).count()) / c);
     std::cerr << "res= " << result << ", elapsed=" << el << ", percall=" << ((double)k2::nsec(el).count()) / c << std::endl;
     std::cout << "res= " << result << ", elapsed=" << el << ", percall=" << ((double)k2::nsec(el).count()) / c << std::endl;
     return 0;
--- src/logging/log.h
+++ src/logging/log.h
@@ -4,18 +4,19 @@
 #include <include/fmt/printf.h>
 #include <include/fmt/compile.h>
 #include <iostream>
+#include <logging/json.h>
 
 namespace k2 {
-    enum LogLevel {
-        VERBOSE,
-        DEBUG,
-        INFO,
-        WARN,
-        ERROR,
-        FATAL
-    };
+enum LogLevel {
+    VERBOSE,
+    DEBUG,
+    INFO,
+    WARN,
+    ERROR,
+    FATAL
+};
 
-    extern LogLevel LOG_LEVEL;
+extern LogLevel LOG_LEVEL;
 }
 
 namespace std {
@@ -73,64 +74,78 @@ inline std::chrono::seconds sec(const Duration& dur) {
     return std::chrono::duration_cast<std::chrono::seconds>(dur);
 }
 
-// this clock source should be used when you don't care just how precise you are with timing
-// and you want to avoid a lot of calls to system's clock.
-// It provides monotonically increasing, thread-local sequence of values and refreshes the
-// system clock when asked.
-struct CachedSteadyClock {
-    typedef Duration duration;
-    typedef Duration::rep rep;
-    typedef Duration::period period;
-    typedef TimePoint time_point;
-    static const bool is_steady = true;
-
-    static time_point now(bool refresh = false) noexcept;
-
-   private:
-    static thread_local TimePoint _now;
+struct Timestamp_ts {
+    uint16_t micros;
+    uint16_t millis;
+    uint8_t secs;
+    uint8_t mins;
+    uint8_t hours;
+    uint16_t days;
 };
 
-// Utility class to keep track of a deadline. Useful for nested requests
-template <typename ClockT = Clock>
-class Deadline {
-   public:
-    Deadline(typename ClockT::duration dur) : _deadline(ClockT::now() + dur) {}
+inline Timestamp_ts toTimestamp_ts(const TimePoint& tp) {
+    auto now = k2::usec(tp.time_since_epoch()).count();
+    auto [quotient1, micros] = std::ldiv(now, 1000);
+    auto [quotient2, millis] = std::ldiv(quotient1, 1000);
+    auto [quotient3, secs] = std::ldiv(quotient2, 60);
+    auto [quotient4, mins] = std::div((int)quotient3, 60); // the quotient is small enough to fit in an int now
+    auto [days, hours] = std::div(quotient4, 24);
+    return {(uint16_t)micros, (uint16_t)millis, (uint8_t)secs, (uint8_t)mins, (uint8_t)hours, (uint16_t)days};
+}
 
-    typename ClockT::duration getRemaining() const {
-        auto now = ClockT::now();
-        if (now >= _deadline) {
-            return typename ClockT::duration(0);
-        }
-        return _deadline - now;
-    }
-
-    bool isOver() const {
-        return ClockT::now() >= _deadline;
-    }
-
-   private:
-    typename ClockT::time_point _deadline;
-};  // class Deadline
-
-inline const char* printTime(TimePoint tp) {
-    // TODO we can use https://en.cppreference.com/w/cpp/chrono/system_clock/to_stream here, but it is a C++20 feature
-    static thread_local char buffer[100];
-    auto now = k2::usec(tp.time_since_epoch());
-    auto microsec = now.count();
-    auto millis = microsec / 1000;
-    microsec -= millis * 1000;
-    auto secs = millis / 1000;
-    millis -= secs * 1000;
-    auto mins = (secs / 60);
-    secs -= (mins * 60);
-    auto hours = (mins / 60);
-    mins -= (hours * 60);
-    auto days = (hours / 24);
-    hours -= (days * 24);
-    std::snprintf(buffer, sizeof(buffer), "%04ld:%02ld:%02ld:%02ld.%03ld.%03ld", (long)days, (long)hours, (long)mins, (long)secs, (long)millis, (long)microsec);
+inline const char* printTime(const TimePoint& tp) {
+    auto ts = toTimestamp_ts(tp);
+    static thread_local char buffer[24];
+    fmt::format_to_n(buffer, sizeof(buffer), "{}", ts);
     return buffer;
 }
+
 }  // namespace k2
+
+template <>
+struct fmt::formatter<k2::Timestamp_ts> {
+    template <typename ParseContext>
+    constexpr auto parse(ParseContext& ctx) {
+        return ctx.begin();
+    }
+
+    template <typename FormatContext>
+    auto format(k2::Timestamp_ts const& ts, FormatContext& ctx) {
+        return fmt::format_to(ctx.out(), FMT_COMPILE("{:04}:{:02}:{:02}:{:02}.{:03}.{:03}"), ts.days, ts.hours, ts.mins, ts.secs, ts.millis, ts.micros);
+    }
+};
+
+template <>
+struct fmt::formatter<k2::Duration> {
+    template <typename ParseContext>
+    constexpr auto parse(ParseContext& ctx) {
+        return ctx.begin();
+    }
+
+    template <typename FormatContext>
+    auto format(k2::Duration const& dur, FormatContext& ctx) {
+        return fmt::format_to(ctx.out(), "{}", k2::TimePoint{} + dur);
+    }
+};
+
+namespace std {
+void inline to_json(nlohmann::json& j, const k2::Duration& obj) {
+    j = nlohmann::json{{"duration", fmt::format("{}", obj)}};
+}
+}
+
+template <>
+struct fmt::formatter<k2::TimePoint> {
+    template <typename ParseContext>
+    constexpr auto parse(ParseContext& ctx) {
+        return ctx.begin();
+    }
+
+    template <typename FormatContext>
+    auto format(k2::TimePoint const& tp, FormatContext& ctx) {
+        return fmt::format_to(ctx.out(), "{}", k2::toTimestamp_ts(tp));
+    }
+};
 
 namespace std {
 inline std::ostream& operator<<(std::ostream& os, const k2::Duration& dur) {
@@ -145,41 +160,35 @@ inline std::ostream& operator<<(std::ostream& os, const k2::TimePoint& tp) {
 }  // namespace std
 
 #define LOG_STREAM std::cout
-#define LOG_FLUSH "\n"
+#define LOG_FLUSH std::flush
 
-#define DO_FLOG(level, module, fmt_str, ...)                                                                  \
-    fmt::print(LOG_STREAM, FMT_STRING("{} - FMT {} - ({}) [{}: {}:{}] " fmt_str "\n"),                                    \
-            "k2::Clock::now()", level, logger._module, __PRETTY_FUNCTION__, __FILE__, __LINE__, ##__VA_ARGS__);
+#define DO_FLOG(level, module, fmt_str, ...)                                                            \
+    fmt::print(LOG_STREAM, FMT_STRING("{} - FMT {} - ({}) [{}: {}:{}] " fmt_str "\n"),                  \
+    "k2::Clock::now()", level, logger._module, __PRETTY_FUNCTION__, __FILE__, __LINE__, ##__VA_ARGS__);
 
 #define LFLOG(level, logger, fmt_str, ...)                    \
     if (logger.isEnabledFor(level)) {                         \
         DO_FLOG(level, logger._module, fmt_str, ##__VA_ARGS__); \
+        LOG_STREAM << LOG_FLUSH; \
     }
 
-#define LFVERBOSE(logger, fmt_str, ...) LFLOG(k2::LogLevel::VERBOSE, logger, fmt_str, __VA_ARGS__);
 #define LFDEBUG(logger, fmt_str, ...) LFLOG(k2::LogLevel::DEBUG, logger, fmt_str, ##__VA_ARGS__);
 #define LFINFO(logger, fmt_str, ...) LFLOG(k2::LogLevel::INFO, logger, fmt_str, __VA_ARGS__);
-#define LFWARN(logger, fmt_str, ...) LFLOG(k2::LogLevel::WARN, logger, fmt_str, __VA_ARGS__);
-#define LFERROR(logger, fmt_str, ...) LFLOG(k2::LogLevel::ERROR, logger, fmt_str, __VA_ARGS__);
-#define LFFATAL(logger, fmt_str, ...) LFLOG(k2::LogLevel::FATAL, logger, fmt_str, __VA_ARGS__);
 
 
 
 #define DO_SLOG(level, module, msg)                                               \
     LOG_STREAM << "k2::Clock::now()" << " - STREAM " << level << " - (" << module \
-               << ") [" << __PRETTY_FUNCTION__ << ": " << __FILE__ << ":" << __LINE__ << "] " << msg << LOG_FLUSH;
+               << ") [" << __PRETTY_FUNCTION__ << ": " << __FILE__ << ":" << __LINE__ << "] " << msg << "\n";
 
 #define LSLOG(level, logger, msg)           \
     if (logger.isEnabledFor(level)) {       \
         DO_SLOG(level, logger._module, msg); \
+        LOG_STREAM << LOG_FLUSH; \
     }
 
-#define LSVERBOSE(logger, msg) LSLOG(k2::LogLevel::VERBOSE, logger, msg);
 #define LSDEBUG(logger, msg) LSLOG(k2::LogLevel::DEBUG, logger, msg);
 #define LSINFO(logger, msg) LSLOG(k2::LogLevel::INFO, logger, msg);
-#define LSWARN(logger, msg) LSLOG(k2::LogLevel::WARN, logger, msg);
-#define LSERROR(logger, msg) LSLOG(k2::LogLevel::ERROR, logger, msg);
-#define LSFATAL(logger, msg) LSLOG(k2::LogLevel::FATAL, logger, msg);
 
 namespace k2 {
 class Logger {
